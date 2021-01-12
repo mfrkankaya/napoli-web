8baad55a0daf959c84296e997243ee0dd4b25f44
Furkan KAYA <mfrkankaya@gmail.com>
2021-01-12 23:14:19 +0300

    add theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,6 +1,7 @@
 export const colors = {
   primary: '#fdd835',
-  text: ['#ffffff', '#eeeeee', '#cccccc', '#999999']
+  text: ['#ffffff', '#eeeeee', '#cccccc', '#999999'],
+  background: ['#000000', '#212121', '#343434', '#454545']
 }
 export const space = [0, 4, 8, 16, 32, 64, 128, 256, 512]
 export const radius = [0, 4, 8, 16, 32, 64, 128, 256, 512]
