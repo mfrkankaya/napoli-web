08646299131850cb14dde414a395b5700b0ec0bb
Furkan KAYA <mfrkankaya@gmail.com>
2021-01-25 00:45:42 +0300

    update prelaod fonts

--- src/components/common/index.jsx
+++ src/components/common/index.jsx
@@ -18,7 +18,7 @@ export const Layout = ({ children, title, description, keywords }) => {
         <meta httpEquiv='Content-Type' content='text/html;charset=UTF-8' />
         <meta name='description' content={description} />
         <meta name='keywords' content={keywords} />
-        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link rel='preload' href='https://fonts.gstatic.com' />
         <link
           href='https://fonts.googleapis.com/css2?family=Merriweather:wght@400;900&family=Oswald:wght@700'
           rel='stylesheet'
