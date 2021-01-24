fb0070e539ad208b5c3781cb11979d4e39141cdc
Furkan KAYA <mfrkankaya@gmail.com>
2021-01-25 01:57:08 +0300

    update move meta tags to _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 // Global Styles
 import '../src/styles/global.css'
 import 'swiper/swiper.min.css'
@@ -11,10 +12,58 @@ import theme from '../src/theme'
 
 export default function App({ Component, pageProps }) {
   return (
-    <AppProvider>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </AppProvider>
+    <React.Fragment>
+      <Head>
+        <meta charSet='utf-8' />
+        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+        <meta
+          name='viewport'
+          content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5,user-scalable=yes'
+        />
+        <meta httpEquiv='Content-Type' content='text/html;charset=UTF-8' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        />
+        <link
+          rel='preload'
+          as='style'
+          href='https://fonts.googleapis.com/css2?family=Merriweather:wght@400;900'
+        />
+        <link
+          rel='stylesheet'
+          media='print'
+          onLoad="this.onload=null;this.removeAttribute('media');"
+          href='https://fonts.googleapis.com/css2?family=Merriweather:wght@400;900'
+        />
+        <noscript>
+          <link
+            rel='stylesheet'
+            href='https://fonts.googleapis.com/css2?family=Merriweather:wght@400;900'
+          />
+        </noscript>
+        <link rel='manifest' href='/manifest.json' />
+        {/* <link
+          href='/icons/icon-192x192.png'
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+        />
+        <link
+          href='/icons/icon-192x192.png'
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+        /> */}
+        <link rel='apple-touch-icon' href='/iconst/icon-256x256.png' />
+        <meta name='theme-color' content='#fdd835' />
+      </Head>
+      <AppProvider>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </AppProvider>
+    </React.Fragment>
   )
 }
--- src/components/common/index.jsx
+++ src/components/common/index.jsx
@@ -9,50 +9,9 @@ export const Layout = ({ children, title, description, keywords }) => {
   return (
     <Fragment>
       <Head>
-        <meta charSet='utf-8' />
-        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-        <meta
-          name='viewport'
-          content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=5,user-scalable=yes'
-        />
-        <meta httpEquiv='Content-Type' content='text/html;charset=UTF-8' />
         <meta name='description' content={description} />
         <meta name='keywords' content={keywords} />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
-        <link
-          rel='preload'
-          as='style'
-          href='https://fonts.googleapis.com/css2?family=Merriweather:wght@400;900'
-        />
-        <link
-          rel='stylesheet'
-          media='print'
-          onLoad="this.onload=null;this.removeAttribute('media');"
-          href='https://fonts.googleapis.com/css2?family=Merriweather:wght@400;900'
-        />
-        <noscript>
-          <link
-            rel='stylesheet'
-            href='https://fonts.googleapis.com/css2?family=Merriweather:wght@400;900'
-          />
-        </noscript>
-
         <title>{title}</title>
-        <link rel='manifest' href='/manifest.json' />
-        {/* <link
-          href='/icons/icon-192x192.png'
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-        />
-        <link
-          href='/icons/icon-192x192.png'
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-        /> */}
-        <link rel='apple-touch-icon' href='/iconst/icon-256x256.png' />
-        <meta name='theme-color' content='#fdd835' />
       </Head>
 
       <Header />
