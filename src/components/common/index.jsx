1f687a0cef2cf1c7c90880fdc1845cb448918b13
Furkan KAYA <mfrkankaya@gmail.com>
2021-01-24 21:44:43 +0300

    update pwa

--- src/components/common/index.jsx
+++ src/components/common/index.jsx
@@ -9,6 +9,12 @@ export const Layout = ({ children, title, description, keywords }) => {
   return (
     <Fragment>
       <Head>
+        <meta charSet='utf-8' />
+        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+        <meta
+          name='viewport'
+          content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'
+        />
         <meta httpEquiv='Content-Type' content='text/html;charset=UTF-8' />
         <meta name='description' content={description} />
         <meta name='keywords' content={keywords} />
@@ -18,6 +24,21 @@ export const Layout = ({ children, title, description, keywords }) => {
           rel='stylesheet'
         />
         <title>{title}</title>
+        <link rel='manifest' href='/manifest.json' />
+        {/* <link
+          href='/icons/icon-192x192.png'
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+        />
+        <link
+          href='/icons/icon-192x192.png'
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+        /> */}
+        <link rel='apple-touch-icon' href='/iconst/icon-256x256.png' />
+        <meta name='theme-color' content='#fdd835' />
       </Head>
 
       <Header />
